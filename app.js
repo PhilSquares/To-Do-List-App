2a635e74ddaa9a60b37ca1ee2d8516fbef894bfd
Phillip Squarcetta <philsquares@Phillips-MacBook-Pro.local>
2023-03-31 10:05:29 -0400

    Added a new form for the delete functionality for each item in the reminders list.

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
 //Setup bodyParser: Must be setup before req.body.newItem can be used below.
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //Tell Express where to find the styles for the application:
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     const day = date.getDate();
-    res.render("list", {listTitle: day, newListItems: items});
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     //Grabs the item from the post request.
     const item = req.body.newItem;
     //If the request comes from our work list, push the value to the workItems array.
-    if(req.body.list === "Work"){
+    if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
     } else {
@@ -34,8 +34,14 @@ app.post("/", function(req, res){
     }
 });
 
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/delete", function (req, res) {
+    const itemIndex = req.body.itemIndex;
+    items.splice(itemIndex, 1);
+    res.redirect("/");
 });
 
 app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
